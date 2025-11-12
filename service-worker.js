edee377412dcb3eb52de16f05ac74512c46c63e3
yoeranmoreel <yoeranmoreel@gmail.com>
2025-11-12 12:52:01 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,19 @@
-const CACHE_NAME = "aanwezigheid-groenewoud-v1";
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json"
-];
-
-// Installatie van de service worker
-self.addEventListener("install", event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open('aanwezigheid-cache').then((cache) => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './icons/icon-192x192.png',
+        './icons/icon-512x512.png'
+      ]);
+    })
   );
 });
 
-// Activeer en verwijder oude caches
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames =>
-      Promise.all(
-        cacheNames
-          .filter(name => name !== CACHE_NAME)
-          .map(name => caches.delete(name))
-      )
-    )
-  );
-});
-
-// Intercept requests en serve cached bestanden als offline
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
